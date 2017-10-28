6689b846890dbeef6e1b257aa19ad5715d6aba6d
2081137s <thearer@googlemail.com>
2017-10-28 22:58:05 +0100

    broken tom

--- helloworld.py
+++ helloworld.py
@@ -34,7 +34,12 @@ WHITE = (255,255,255)
 RED = (255,0,0)
 
 
-
+class Background(pygame.sprite.Sprite):
+	def __init__(self, image_file, location):
+		pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+		self.image = pygame.image.load(image_file)
+		self.rect = self.image.get_rect()
+		self.rect.left, self.rect.top = location
 
 
 def left_dab():
@@ -54,13 +59,6 @@ def random_dab():
 		right_dab()
 
 def intro():
-	class Background(pygame.sprite.Sprite):
-		def __init__(self, image_file, location):
-			pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
-			self.image = pygame.image.load(image_file)
-			self.rect = self.image.get_rect()
-			self.rect.left, self.rect.top = location
-
 	end_it=False
 	BackGround = Background('Untitled.png', [0,0])
 	while (end_it==False):
@@ -76,6 +74,7 @@ def intro():
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				mouse_pos = pygame.mouse.get_pos() # gets mouse position
 				if start.collidepoint(mouse_pos):
+					print("here")
 					end_it = True
 				if quit.collidepoint(mouse_pos):
 					pygame.quit()
@@ -85,70 +84,99 @@ def intro():
 			DISPLAYSURF.blit(quittext, (400,400))
 			pygame.display.flip()
 
-#intro
-intro()
+def outro():
+	end_it=False
+	BackGround = Background('Untitled2.png', [0,0])
+	while (end_it==False):
+		for event in pygame.event.get():
+			DISPLAYSURF.fill([255, 255, 255])
+			DISPLAYSURF.blit(BackGround.image, BackGround.rect)
+			start = pygame.draw.rect(DISPLAYSURF, [0, 0, 0], (200, 400, 90, 30))
+			quit = pygame.draw.rect(DISPLAYSURF, [0, 0, 0], (400, 400, 70, 30))
+			myfont=pygame.font.SysFont("Britannic Bold", 40)
+			title=myfont.render("HAHA UNLUCKY MATE", 1, (255, 0, 0))
+			quittext=myfont.render("QUIT", 1, (255, 0, 0))
+			starttext=myfont.render("MENU", 1, (255, 0, 0))
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				mouse_pos = pygame.mouse.get_pos() # gets mouse position
+				if start.collidepoint(mouse_pos):
+					end_it = True
+				if quit.collidepoint(mouse_pos):
+					pygame.quit()
+					sys.exit()
+			DISPLAYSURF.blit(title,(200,200))
+			DISPLAYSURF.blit(starttext,(200,400))
+			DISPLAYSURF.blit(quittext, (400,400))
+			pygame.display.flip()
 
-#initialising surface
-DISPLAYSURF.fill(WHITE)
-#body:
-pygame.draw.rect(DISPLAYSURF, RED, (body_top_left_x,body_top_left_y,body_width,body_height))
-#head:
-pygame.draw.circle(DISPLAYSURF, RED, (head_x, head_y),head_rad, 0)
-#left upper arm:
-pygame.draw.line(DISPLAYSURF, BLACK, (left_should_x,left_should_y),(left_elbow_x,left_elbow_y),arm_thiccness)
-#lower left arm:
-pygame.draw.line(DISPLAYSURF, BLACK, (left_elbow_x, left_elbow_y),(left_hand_x,left_hand_y),arm_thiccness)
+def main_game():
+	print("here")
+	#initialising surface
+	DISPLAYSURF.fill(WHITE)
+	#body:
+	pygame.draw.rect(DISPLAYSURF, RED, (body_top_left_x,body_top_left_y,body_width,body_height))
+	#head:
+	pygame.draw.circle(DISPLAYSURF, RED, (head_x, head_y),head_rad, 0)
+	#left upper arm:
+	pygame.draw.line(DISPLAYSURF, BLACK, (left_should_x,left_should_y),(left_elbow_x,left_elbow_y),arm_thiccness)
+	#lower left arm:
+	pygame.draw.line(DISPLAYSURF, BLACK, (left_elbow_x, left_elbow_y),(left_hand_x,left_hand_y),arm_thiccness)
 
-counter  = 0
+	counter  = 0
 
-# initialize font; must be called after 'pygame.init()' to avoid 'Font not Initialized' error
-myfont = pygame.font.SysFont("monospace", 30)
+	# initialize font; must be called after 'pygame.init()' to avoid 'Font not Initialized' error
+	myfont = pygame.font.SysFont("monospace", 30)
 
-# render text
+	# render text
 
 
-#timer
-seconds = 0
-start_ticks = pygame.time.get_ticks()
+	#timer
+	seconds = 0
+	start_ticks = pygame.time.get_ticks()
+	game_over = False
+	while True:
+	    #clear screen
+	    DISPLAYSURF.fill(WHITE)
+	    counterLabel = myfont.render("SCORE: "+str(counter), 1, RED)
+	    DISPLAYSURF.blit(counterLabel, (5, 5))
+	    #redraw static images
+	    #body:
+	    pygame.draw.rect(DISPLAYSURF, RED, (body_top_left_x,body_top_left_y,body_width,body_height))
+	    #head:
+	    pygame.draw.circle(DISPLAYSURF, RED, (head_x, head_y),head_rad, 0)
+	    #left upper arm:
+	    pygame.draw.line(DISPLAYSURF, BLACK, (left_should_x,left_should_y),(left_elbow_x,left_elbow_y),arm_thiccness)
+	    #lower left arm:
+	    pygame.draw.line(DISPLAYSURF, BLACK, (left_elbow_x, left_elbow_y),(left_hand_x,left_hand_y),arm_thiccness)
+
+	    if game_over == False:
+
+	        seconds = (pygame.time.get_ticks()-start_ticks)/1000
+	        if seconds>29:
+	            game_over = True
+	        timer_label = myfont.render("TIME REMAINING: "+str(30-seconds),1,RED)
+	        DISPLAYSURF.blit(timer_label, (screen_width-400,5))
+
+	        for event in pygame.event.get():
+	            if event.type == QUIT:
+	                pygame.quit()
+	                sys.exit()
+	            elif event.type == KEYDOWN:
+	                if event.key == K_q:
+	                    counter+=1
+	                    #raise left elbow
+	                    print "q pressed"
+	                    print "test"
+	                    #left_elbow_x = arm_length*math.cos(math.pi/120) + left_should_x
+	                    #left_elbow_y = arm_length*math.sin(math.pi/120) + left_should_y
+	    else:
+	        #display end screen
+	        break
+
+
+    	pygame.display.update()
 
 while True:
-    #clear screen
-    DISPLAYSURF.fill(WHITE)
-    counterLabel = myfont.render("SCORE: "+str(counter), 1, RED)
-    DISPLAYSURF.blit(counterLabel, (5, 5))
-    #redraw static images
-    #body:
-    pygame.draw.rect(DISPLAYSURF, RED, (body_top_left_x,body_top_left_y,body_width,body_height))
-    #head:
-    pygame.draw.circle(DISPLAYSURF, RED, (head_x, head_y),head_rad, 0)
-    #left upper arm:
-    pygame.draw.line(DISPLAYSURF, BLACK, (left_should_x,left_should_y),(left_elbow_x,left_elbow_y),arm_thiccness)
-    #lower left arm:
-    pygame.draw.line(DISPLAYSURF, BLACK, (left_elbow_x, left_elbow_y),(left_hand_x,left_hand_y),arm_thiccness)
-
-    if game_over == False:
-
-        seconds = (pygame.time.get_ticks()-start_ticks)/1000
-        if seconds>29:
-            game_over = True
-        timer_label = myfont.render("TIME REMAINING: "+str(30-seconds),1,RED)
-        DISPLAYSURF.blit(timer_label, (screen_width-400,5))
-
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == KEYDOWN:
-                if event.key == K_q:
-                    counter+=1
-                    #raise left elbow
-                    print "q pressed"
-                    print "test"
-                    left_elbow_x = arm_length*math.cos(math.pi/120) + left_should_x
-                    left_elbow_y = arm_length*math.sin(math.pi/120) + left_should_y
-    else:
-        #display end screen
-        break
-
-
-    pygame.display.update()
+	intro()
+	main_game()
+	outro()
