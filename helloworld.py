7ea934ca2050f44dc1341d7a3bcff17254a148f6
Rebecca Martin <rebyk511@msn.com>
2017-10-28 21:28:13 +0100

    timer implemented

--- helloworld.py
+++ helloworld.py
@@ -21,9 +21,6 @@ left_hand_x = 50
 left_hand_y = 250
 arm_thiccness = 30
 
-#timer
-start_ticks = pygame.time.get_ticks()
-
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('dwab dwab revolution 2017')
@@ -53,10 +50,28 @@ myfont = pygame.font.SysFont("monospace", 15)
 label = myfont.render(str(counter), 1, RED)
 DISPLAYSURF.blit(label, (5, 5))
 
+#timer
+seconds = 0
+start_ticks = pygame.time.get_ticks()
+
 while True:
+    #clear screen
+    DISPLAYSURF.fill(WHITE)
+    #redraw static images
+    #body:
+    pygame.draw.rect(DISPLAYSURF, RED, (body_top_left_x,body_top_left_y,body_width,body_height))
+    #head:
+    pygame.draw.circle(DISPLAYSURF, RED, (head_x, head_y),head_rad, 0)
+    #left upper arm:
+    pygame.draw.line(DISPLAYSURF, BLACK, (left_should_x,left_should_y),(left_elbow_x,left_elbow_y),arm_thiccness)
+    #lower left arm:
+    pygame.draw.line(DISPLAYSURF, BLACK, (left_elbow_x, left_elbow_y),(left_hand_x,left_hand_y),arm_thiccness)
+
     seconds = (pygame.time.get_ticks()-start_ticks)/1000
     if seconds>30:
         break
+    timer_label = myfont.render(str(seconds),1,RED)
+    DISPLAYSURF.blit(timer_label, (screen_width-30,5))
     print seconds
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -65,7 +80,7 @@ while True:
         elif event.type == KEYDOWN:
             if event.key == K_q:
                 counter+=1
-                label = myfont.render(str(counter), 1, (255,0,0))
+                label = myfont.render(str(counter), 1, RED)
                 DISPLAYSURF.blit(label, (5, 5))
                 #raise left elbow
                 print "q pressed"
