067c185c3951708b7263a08155e5c1cb6c2734a2
DuncanMilne <duncan.milne@outlook.com>
2017-10-28 23:07:47 +0100

    fixing left and right elbow nonsense

--- helloworld.py
+++ helloworld.py
@@ -17,13 +17,10 @@ head_y = screen_height/2 - head_rad
 arm_length = 80
 left_should_x = body_top_left_x
 left_should_y = body_top_left_y
-left_elbow_x = left_should_x - 20
-left_elbow_y = left_should_y + arm_length
 left_hand_x = 50
 left_hand_y = 250
 arm_thiccness = 30
 game_over = False
-direction_of_dab = "right"
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((screen_width,screen_height) ,0,24)
@@ -33,8 +30,9 @@ pygame.display.set_caption('dwab dwab revolution 2017')
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 RED = (255,0,0)
-
-<<<<<<< HEAD
+myfont=pygame.font.SysFont("Britannic Bold", 40)
+counter  = 0
+start_ticks = pygame.time.get_ticks()
 
 class Background(pygame.sprite.Sprite):
 	def __init__(self, image_file, location):
@@ -52,9 +50,6 @@ def right_dab():
     dab_label = myfont.render("RIGHT DAB",1, RED)
     DISPLAYSURF.blit(dab_label, (300, 50))
 
-
-=======
->>>>>>> 591963ef9045df1e8ece46d0c89c8864ac142968
 def random_dab():
     random_int = randint(0, 9)
     if random_int > 4:
@@ -71,7 +66,6 @@ def intro():
 			DISPLAYSURF.blit(BackGround.image, BackGround.rect)
 			start = pygame.draw.rect(DISPLAYSURF, [0, 0, 0], (200, 400, 90, 30))
 			quit = pygame.draw.rect(DISPLAYSURF, [0, 0, 0], (400, 400, 70, 30))
-			myfont=pygame.font.SysFont("Britannic Bold", 40)
 			title=myfont.render("DWABP DWABP REVOLUTION 2017", 1, (255, 0, 0))
 			quittext=myfont.render("QUIT", 1, (255, 0, 0))
 			starttext=myfont.render("START", 1, (255, 0, 0))
@@ -114,6 +108,9 @@ def outro():
 			pygame.display.flip()
 
 def main_game():
+	
+	left_elbow_x = left_should_x - 20
+	left_elbow_y = left_should_y + arm_length
 	print("here")
 	#initialising surface
 	DISPLAYSURF.fill(WHITE)
@@ -126,7 +123,6 @@ def main_game():
 	#lower left arm:
 	pygame.draw.line(DISPLAYSURF, BLACK, (left_elbow_x, left_elbow_y),(left_hand_x,left_hand_y),arm_thiccness)
 
-	counter  = 0
 
 	# initialize font; must be called after 'pygame.init()' to avoid 'Font not Initialized' error
 	myfont = pygame.font.SysFont("monospace", 30)
@@ -136,8 +132,11 @@ def main_game():
 
 	#timer
 	seconds = 0
-	start_ticks = pygame.time.get_ticks()
 	game_over = False
+	foundNumbers = []
+
+	counter = 0
+	direction_of_dab = "right"
 	while True:
 	    #clear screen
 	    DISPLAYSURF.fill(WHITE)
@@ -158,6 +157,17 @@ def main_game():
 	        seconds = (pygame.time.get_ticks()-start_ticks)/1000
 	        if seconds>29:
 	            game_over = True
+	        if seconds%3==0 and seconds not in foundNumbers:
+	        	direction_of_dab = random_dab()
+	        	foundNumbers.append(seconds)
+	        
+	        if direction_of_dab == "left":
+			    dab_label = myfont.render("LEFT DAB",1, RED)
+			    DISPLAYSURF.blit(dab_label, (300, 50))
+	        else:
+			    dab_label = myfont.render("RIGHT DAB",1, RED)
+			    DISPLAYSURF.blit(dab_label, (300, 50))
+
 	        timer_label = myfont.render("TIME REMAINING: "+str(30-seconds),1,RED)
 	        DISPLAYSURF.blit(timer_label, (screen_width-400,5))
 
@@ -171,69 +181,14 @@ def main_game():
 	                    #raise left elbow
 	                    print "q pressed"
 	                    print "test"
-	                    #left_elbow_x = arm_length*math.cos(math.pi/120) + left_should_x
-	                    #left_elbow_y = arm_length*math.sin(math.pi/120) + left_should_y
+	                    left_elbow_x = arm_length*math.cos(math.pi/120) + left_should_x
+	                    left_elbow_y = arm_length*math.sin(math.pi/120) + left_should_y
 	    else:
 	        #display end screen
 	        break
 
 
-    	pygame.display.update()
-
-foundNumbers = []
-
-while True:
-    #clear screen
-    DISPLAYSURF.fill(WHITE)
-    counterLabel = myfont.render("SCORE: "+str(counter), 1, RED)
-    DISPLAYSURF.blit(counterLabel, (5, 5))
-    #redraw static images
-    #body:
-    pygame.draw.rect(DISPLAYSURF, RED, (body_top_left_x,body_top_left_y,body_width,body_height))
-    #head:
-    pygame.draw.circle(DISPLAYSURF, RED, (head_x, head_y),head_rad, 0)
-    #left upper arm:
-    pygame.draw.line(DISPLAYSURF, BLACK, (left_should_x,left_should_y),(left_elbow_x,left_elbow_y),arm_thiccness)
-    #lower left arm:
-    pygame.draw.line(DISPLAYSURF, BLACK, (left_elbow_x, left_elbow_y),(left_hand_x,left_hand_y),arm_thiccness)
-
-    if game_over == False:
-
-        seconds = (pygame.time.get_ticks()-start_ticks)/1000
-        if seconds>29:
-            game_over = True
-        if seconds%3==0 and seconds not in foundNumbers:
-        	direction_of_dab = random_dab()
-        	foundNumbers.append(seconds)
-        
-        if direction_of_dab == "left":
-		    dab_label = myfont.render("LEFT DAB",1, RED)
-		    DISPLAYSURF.blit(dab_label, (300, 50))
-        else:
-		    dab_label = myfont.render("RIGHT DAB",1, RED)
-		    DISPLAYSURF.blit(dab_label, (300, 50))
-
-        timer_label = myfont.render("TIME REMAINING: "+str(30-seconds),1,RED)
-        DISPLAYSURF.blit(timer_label, (screen_width-400,5))
-
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == KEYDOWN:
-                if event.key == K_q:
-                    counter+=1
-                    #raise left elbow
-                    print "q pressed"
-                    print "test"
-                    left_elbow_x = arm_length*math.cos(math.pi/120) + left_should_x
-                    left_elbow_y = arm_length*math.sin(math.pi/120) + left_should_y
-    else:
-        #display end screen
-        break
-
-
-    pygame.display.update()
+	    pygame.display.update()
 
 intro()
 main_game()
